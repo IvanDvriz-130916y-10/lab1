72124e5abf59b8ce3e494f97d618ae2e3cacb025
Иван <dvornikov1ks@yandex.ru>
2024-01-12 19:48:24 +0500

    Merge remote-tracking branch 'original/master'

--- src/ЛР1/Example1.java
+++ src/ЛР1/Example1.java
@@ -1,8 +1,9 @@
 package ЛР1;
 
+import java.util.Scanner;
 public class Example1 {
-    public static void main(String[] args) {
-        System.out.println("Привет мир!");
+        public static void main(String[] args) {
+            System.out.println("Привет мир!");
+        }
     }
-}
 
